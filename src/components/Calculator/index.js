e5c949af74ac62eb4ad9a2183966e28f366804b9
ChrisColley <chris.s.colley@gmail.com>
2021-09-10 15:53:15 +1000

    Fixed issues around display of negative decimal numbers and also refactored to create an evaluate() function for code that was previously repeated in handleOperatorClick and handleEqualsClick

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -52,6 +52,38 @@ const Calculator = () => {
     }
   };
 
+  const evaluate = () => {
+    // evaluate concatenated total and current expression
+    // eslint-disable-next-line no-eval
+    const result =
+      Math.round(eval(`${total}${current}`) * 100000000000000) /
+      100000000000000;
+    // if result is too positive or too negative return appropriate message
+    if (result > 99999999999999 || result < -9999999999999) {
+      return "TOO LONG";
+    }
+    // if result as a string has too many characters
+    else if (result.toString().length > 14) {
+      // count number of non-digits (i.e. "-" or "." characters)
+      const nonDigitCount = (result.toString().match(/[^\d]/g) || []).length;
+      // count number of trailing zeros if any
+      const trailingZerosCount =
+        -Math.floor(Math.log10(Math.abs(result))) > 0
+          ? -Math.floor(Math.log10(Math.abs(result)))
+          : 0;
+      // assign to a new variable, precise, the result as a string with precison of
+      // 14 minus nonDigitCount minus trailingZerosCount so that it fits on screen
+      const precise = Number.parseFloat(result).toPrecision(
+        14 - nonDigitCount - trailingZerosCount
+      );
+      return precise;
+    }
+    // else return result as string
+    else {
+      return result.toString();
+    }
+  };
+
   const handleOperatorClick = (e) => {
     // if current ends with "+", "-", "*" or "/" operator
     if (/[+\-*/]$/.test(current)) {
@@ -66,7 +98,11 @@ const Calculator = () => {
         return;
       }
     }
-    // else if current doesn't end with "+", "-", "*" or "/" operator but total is still default
+    // if too long message is already displayed do nothing
+    else if (current === "TOO LONG") {
+      return;
+    }
+    // if current doesn't end with "+", "-", "*" or "/" operator but total is still default
     // empty string then set total to current and set current to operator button clicked
     else if (total === "") {
       setTotal(current);
@@ -75,38 +111,15 @@ const Calculator = () => {
     }
     // else evaluate concatenated total and current expression
     else {
-      // evaluate concatenated total and current expression
-      // eslint-disable-next-line no-eval
-      const result =
-        Math.round(eval(`${total}${current}`) * 100000000000000) /
-        100000000000000;
-      // result has too many digits display appropriate message
-      if (result >= 99999999999999) {
+      const evaluation = evaluate();
+      // if evaluate() returns "TOO LONG" set total to empty string and current to "TOO LONG"
+      if (evaluation === "TOO LONG") {
         setTotal("");
-        setCurrent("TOO LONG");
-      }
-      // if result as a string has too many characters
-      else if (result.toString().length > 14) {
-        // count number of non-digits (i.e. "-" or "." characters)
-        const nonDigitCount = (result.toString().match(/[^\d]/g) || []).length;
-        // count number of trailing zeros if any
-        const trailingZerosCount =
-          -Math.floor(Math.log10(result)) > 0
-            ? -Math.floor(Math.log10(result))
-            : 0;
-        // assign to a new variable, precise, result as a string with precison of
-        // 14 minus nonDigitCount and trailingZerosCount so that it fits on screen
-        const precise = Number.parseFloat(result).toPrecision(
-          14 - nonDigitCount - trailingZerosCount
-        );
-        // set total to precise
-        setTotal(precise);
-        // set current to operator clicked
-        setCurrent(e.target.value);
-      }
-      // else set set total to result and set current to operator clicked
+        setCurrent(evaluation);
+      } 
+      // otherwise set total to the evaluation and set current to the operator clicked
       else {
-        setTotal(result.toString());
+        setTotal(evaluation);
         setCurrent(e.target.value);
       }
     }
@@ -116,47 +129,18 @@ const Calculator = () => {
     // if current ends with an operator do nothing
     if (/[+\-*/]$/.test(current)) {
       return;
-    } 
-    // if total is still default empty string do nothing 
+    }
+    // if total is still default empty string do nothing
     else if (total === "") {
       return;
     }
     // else evaluate concatenated total and current expression
     else {
-      // evaluate concatenated total and current expression
-      // eslint-disable-next-line no-eval
-      const result =
-        Math.round(eval(`${total}${current}`) * 100000000000000) /
-        100000000000000;
-      // if result is too large display appropriate message
-      if (result >= 99999999999999) {
-        setTotal("");
-        setCurrent("TOO LONG");
-      }
-      // if result as a string has too many characters
-      else if (result.toString().length > 14) {
-        // count number of non-digits (i.e. "-" or "." characters)
-        const nonDigitCount = (result.toString().match(/[^\d]/g) || []).length;
-        // count number of trailing zeros if any
-        const trailingZerosCount =
-          -Math.floor(Math.log10(result)) > 0
-            ? -Math.floor(Math.log10(result))
-            : 0;
-        // assign to a new variable, precise, the result as a string with precison of
-        // 14 minus nonDigitCount minus trailingZerosCount so that it fits on screen
-        const precise = Number.parseFloat(result).toPrecision(
-          14 - nonDigitCount - trailingZerosCount
-        );
-        // set total to empty string
-        setTotal("");
-        // set current to precise
-        setCurrent(precise);
-      }
-      // else set total to empty string and set current to precise
-      else {
-        setTotal("");
-        setCurrent(result.toString());
-      }
+      const evaluation = evaluate();
+      // Set total to empty string
+      setTotal("");
+      // Set current to the evaluation
+      setCurrent(evaluation);
     }
   };
 
