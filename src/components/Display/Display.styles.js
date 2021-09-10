c0d71db97443c0c2ce34cd86218a583b47abb751
ChrisColley <chris.s.colley@gmail.com>
2021-09-10 12:41:06 +1000

    Made adjustment to display css to increase spacing between Total and Current components so that it displays better on my phone

--- src/components/Display/Display.styles.js
+++ src/components/Display/Display.styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   width: 100%;
-  height: 106px;
+  height: 116px;
   padding: 16px 7px;
   text-align: right;
   background-color: var(--defaultColor);
@@ -20,7 +20,7 @@ export const Total = styled.h2`
 
 export const Current = styled.h1`
   width: 100%;
-  margin: 0;
+  margin-top: 10px;
   font-family: 'Digital-7 Mono', 'Roboto', sans-serif;
   font-weight: normal; font-style: normal;
   font-size: 44px;
